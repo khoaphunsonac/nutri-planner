c19efaaa08efb04fe018dfe20810b458b64011d6
Tran Tien Dat <trtdat161@gmail.com>
2025-08-06 11:53:42 +0700

    layout mới nhất

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -49,7 +49,7 @@
         <a href="{{ route('ingredients.index') }}"><i class="bi bi-basket"></i> Ingredients</a>
         <a href=""><i class="bi bi-people"></i> Users</a>
         <a href=""><i class="bi bi-tags"></i> Tags</a> <!-- Đã thêm mục này -->
-        <a href="""><i class="bi bi-envelope"></i> Contacts</a>
+        <a href=""><i class="bi bi-envelope"></i> Contacts</a>
         <a href=""><i class="bi bi-chat-dots"></i> Feedbacks</a>
         <a href=""><i class="bi bi-list-ul"></i> Diet Types</a>
         <a href=""><i class="bi bi-grid"></i> Meal Types</a>
