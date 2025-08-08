6da23c7d93338c8adaf263fed30ee4a9a53fa847
unknown <dipthanhhong1020@gmail.com>
2025-08-08 17:12:08 +0700

    cập nhật form

--- app/Http/Controllers/Admin/TagController.php
+++ app/Http/Controllers/Admin/TagController.php
@@ -22,7 +22,7 @@ class TagController extends BaseController
     public function index(Request $request){
         $id = $request->id;
         $tags = TagModel::all();
-        $tagMeal  = TagModel::with('meals')->paginate(10);
+        $tagMeal  = TagModel::with('meals')->paginate(10,['*'],'mappingmeal');
         $itemMeal = TagModel::with('meals')->get();
         $allMeals =  MealModel::select('id','name')->get();
         $params = $request->all();
@@ -52,7 +52,7 @@ class TagController extends BaseController
         }
         $usageRate =  $totalTags > 0 ? round(($activeTags/$totalTags) *100) . '%' : '0%';
         // $item = $query->orderBy('id','desc')->get();
-        $item = $query->orderBy('id','desc')->paginate(10);
+        $item = $query->orderBy('id','desc')->paginate(10,['*'],'tags');
         // Tính chỉ số bắt đầu đếm ngược (số lớn nhất trên trang hiện tại)
         $total = $item->total();
         $perPage = $item->perPage();
@@ -78,6 +78,7 @@ class TagController extends BaseController
 
     public function show($id,Request $request){
         $tag = TagModel::with('meals')->findOrFail($id);
+        
         return view($this->pathViewController.'show',[
             
             'item'=>$tag,
@@ -87,10 +88,22 @@ class TagController extends BaseController
     public function form(Request $request){
          $id=$request->id;
         $item = $id ? TagModel::with('meals')->findOrFail($id) : null;
-        $allMeals = MealModel::select('id', 'name')->get();
+
+        // lay ds mon an co phan trang
+         $meals = MealModel::orderBy('name','asc')->paginate(20,['*'],'meals_page');
+         //neu dang edit lay mang ID mon an da duoc gan
+         $selectedMeals = [];
+         
+         if($item){
+            foreach($item->meals as $meal){
+                $selectedMeals[] = $meal->id; //lay id cua tung mon an gan
+            }
+         }
+        
         return view($this->pathViewController.'form',[
             'item'=>$item,
-            "allMeals"=>$allMeals,
+            'meals'=>$meals,
+            'selectedMeals'=>$selectedMeals,
         ]);
     }
 
@@ -98,22 +111,25 @@ class TagController extends BaseController
 
     public function save(TagRequest $request){
         $id = $request->id;
-        $params = $request->except(['meals']); // chỉ lấy những field liên quan đến tag
-         $mealsInput = $request->input('meals', '');
-        // $mealIds = $mealsInput ?  explode(',', $mealsInput) : [];
-        $mealIds = collect(explode(',', $mealsInput))->filter()->all(); // Loại bỏ phần tử rỗng
+        $params = $request->all(); 
+        $mealIds = $request->input('meals',[]); // ds mon duoc chon, con k chọn là mảng rỗng
+       
         if($id){
-            $tag = TagModel::withTrashed()->findOrFail($id);
+            $tag = TagModel::findOrFail($id);
             $tag->update($params);
-            $message = "Cập nhật Thẻ `{$tag->name}` thành công";
+            //cap nhat mapping
+            $tag->meals()->sync($mealIds);
+           
+            return redirect()->route('tags.index',['id'=>$tag->id])->with('success',"Cập nhật Dị ứng '{$tag->name}' thành công");
         }else{
             
             $tag =TagModel::create($params);
-             $message = "Thêm Thẻ '{$tag->name}' thành công";
-
+            if(!empty($mealIds)){
+                $tag->meals()->sync($mealIds);
+            }
+             return $tag;
+            return redirect()->route('tags.index')->with('success', "Thêm mới Dị ứng '{$tag->name}' thành công");
         }
-        $tag->meals()->sync($mealIds);
-         return redirect()->route('tags.index')->with('success', $message);
     }
     
     public function destroy($id){
