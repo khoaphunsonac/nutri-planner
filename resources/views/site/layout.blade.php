08607dfd5c2902cb40d73d599baf9c48ceb7fedb
Tran Tien Dat <trtdat161@gmail.com>
2025-08-09 13:58:43 +0700

    layout UI for user

--- resources/views/site/layout.blade.php
+++ resources/views/site/layout.blade.php
@@ -64,18 +64,21 @@
         <div class="slide">
             <img src="https://fitfood.vn/img/1920x800/images/nen-web-16950958311174.webp?f=jpg" alt="meal">
             <p>TRẢI NGHIỆM BỮA ĂN SẠCH
+                <br>
                 <strong>TƯƠI NGON GIÀU DINH DƯỠNG</strong>
             </p>
         </div>
         <div class="slide">
             <img src="https://fitfood.vn/img/1920x800/images/nen-final-1-1695095846415.webp?f=jpg" alt="meal">
             <p>KẾ HOẠCH BỮA ĂN HÀNG TUẦN
+                <br>
                 <strong>CHO MỘT LỐI SỐNG LÀNH MẠNH</strong>
             </p>
         </div>
         <div class="slide">
             <img src="https://fitfood.vn/img/1920x800/images/nen-final-2-16947528537416.webp?f=jpg" alt="meal">
             <p>GIẢI PHÁP HEALTHY FOOD
+                <br>
                 <strong>BỮA ĂN SẠCH</strong>
             </p>
         </div>
