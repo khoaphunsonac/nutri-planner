84a38b853da0ca266755b5afd1bc12f351e12198
AnhKhoa090606 <lekhoa090606@gmail.com>
2025-08-09 10:44:09 +0700

    chỉnh lại contact

--- app/Http/Controllers/Admin/ContactController.php
+++ app/Http/Controllers/Admin/ContactController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
+use App\Http\Controllers\Controller;
 use App\Models\ContactModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\View;
