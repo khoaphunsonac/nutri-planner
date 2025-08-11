4d520f75a2afe76ec2253c52a372a274d36c3296
Tran Tien Dat <trtdat161@gmail.com>
2025-08-11 21:50:06 +0700

    trang home lúc 21:50

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -5,8 +5,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
-    <title>Fitfood Admin</title>
+    {{-- favicon --}}
+    <link rel="apple-touch-icon" sizes="57x57" href="{{ asset('assets/admin/img/avatar/favicon.ico/apple-icon-57x57.png') }}">
+    <link rel="apple-touch-icon" sizes="60x60" href="{{ asset('assets/admin/img/avatar/favicon.ico/apple-icon-60x60.png') }}">
+    <link rel="apple-touch-icon" sizes="72x72" href="{{ asset('assets/admin/img/avatar/favicon.ico/apple-icon-72x72.png') }}">
+    <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('assets/admin/img/avatar/favicon.ico/apple-icon-76x76.png') }}">
+    <link rel="apple-touch-icon" sizes="114x114" href="{{ asset('assets/admin/img/avatar/favicon.ico/apple-icon-114x114.png') }}">
+    <link rel="apple-touch-icon" sizes="120x120" href="{{ asset('assets/admin/img/avatar/favicon.ico/apple-icon-120x120.png') }}">
+    <link rel="apple-touch-icon" sizes="144x144" href="{{ asset('assets/admin/img/avatar/favicon.ico/apple-icon-114x114.png') }}">
+    <link rel="apple-touch-icon" sizes="152x152" href="{{ asset('assets/admin/img/avatar/favicon.ico/apple-icon-152x152.png') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/admin/img/avatar/favicon.ico/apple-icon-180x180.png') }}">
+    <link rel="icon" type="image/png" sizes="192x192"  href="{{ asset('assets/admin/img/avatar/favicon.ico/android-icon-192x192.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/admin/img/avatar/favicon.ico/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('assets/admin/img/avatar/favicon.ico/favicon-96x96.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/admin/img/avatar/favicon.ico/favicon-16x16.png') }}">
+    <link rel="manifest" href="/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+    <title>Nutri Planner</title>
 
     <!-- Bootstrap, Icons, Font -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -62,8 +79,9 @@
     <aside class="sidebar" id="sidebar">
         <a href="{{ route('dashboard') }}" class="text-decoration-none text-dark">
             <div class="sidebar-header p-3">
-                <h5 class="m-0">🍴 Fitfood</h5>
-                <small>Fitfood Quản trị</small>
+                {{-- <h5 class="m-0">🍴 Fitfood</h5>
+                <small>Fitfood Quản trị</small> --}}
+                    <img src="{{ asset('assets/admin/img/avatar/logochinh.png') }}" alt="logo fitfood" style="width: 100%; height: 40px;">
             </div>
         </a>
         {{-- hiện vãn chưa có link được nha anh em test link bên anh em đi --}}
