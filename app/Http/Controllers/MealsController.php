050059e0bc2c7882a67bcc96d91d47a69ed38b07
unknown <dipthanhhong1020@gmail.com>
2025-08-12 17:28:38 +0700

    thêm search và fillter cho user meal

--- app/Http/Controllers/MealsController.php
+++ app/Http/Controllers/MealsController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\SearchFillterRequest;
+use App\Models\AllergenModel;
+use App\Models\DietTypeModel;
 use App\Models\MealModel;
+use App\Models\MealTypeModel;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Http\Request;
@@ -14,26 +18,93 @@ class MealsController extends BaseController
     public $controllerName = "meals";
     public $pathViewController = "site.meals.";
 
-    public function index(Request $request){
+    public function index(SearchFillterRequest $request){
+
         $params = $request->all();
+
         $search = $params['search'] ?? '';
-        $tab  = $params['tab'] ?? 'thuc-don';
+        
         $mealType = $params['meal_type'] ?? '';
+        $caloriesMin = $params['calories_min'] ?? '';
+        $caloriesMax = $params['calories_max'] ?? '';
+        $allergen = $params['allergen'] ?? '';
+        $diet = $params['diet'] ?? '';
 
-        
+
+        $dietTypes = DietTypeModel::all();
+        $mealTypes = MealTypeModel::all();
+        $allergens = AllergenModel::all();
+
         // khởi tạo query  lấy từ model gốc
-        $meals = MealModel::with('recipeIngredients.ingredient')->orderBy('id','desc');
-        //lọc theo tên
+        $meals = MealModel::with('recipeIngredients.ingredient')
+                            ->withSum('recipeIngredients as total_calories', 'total_calo') // Sửa đổi ở đây
+                            ->orderBy('id','desc' );
+
+        // Gom điều kiện tìm kiếm thành mảng cho bên hiển thị form k tìm thấy
+            $searchConditions = [];
+
+        //Tìm theo tên, nguyên liệu, diet type
         if(!empty($search)){
-            $meals = $meals->where('name','like',"%$search%");
+            $meals = $meals->where(function($query) use ($search){
+                $query->where('name','like',"%$search%")
+                    ->orwhereHas('ingredients',function($q) use($search){
+                        $q->where('name','like',"%$search%");
+                    })
+                    ->orwhereHas('dietType',function($q) use($search){
+                        $q->where('name','like',"%$search%");
+                    });
+            });
+            $searchConditions[] = 'từ khóa " ' . $search. ' "';
         }
-        //lọc theo meal
+
+        //lọc theo diet
+        if(!empty($diet)){
+            $dietName = DietTypeModel::find($diet)?->name ?? $diet; // Lấy tên diet từ DB
+            $meals = $meals->whereHas('dietType',function($q) use($diet){
+                        $q->where('diet_type.id',$diet);
+                    });
+                if($dietName){
+                    $searchConditions[] = 'chế độ ăn " ' . $dietName. ' "';
+
+                }
+
+           
+        }
+
+        //lọc theo allergen
+        if(!empty($allergen)){
+            $allergentName = AllergenModel::find($allergen)?->name ?? $allergen; // Lấy tên diet từ DB
+            $meals = $meals->whereHas('allergens',function($q) use($allergen){
+                        $q->where('allergens.id',$allergen);
+                    });
+               if($allergentName){
+                    $searchConditions[] = 'chất dị ứng " ' . $allergentName. ' "';
+
+                }
+
+            
+        }
+
+        //lọc theo mealtype
         if(!empty($mealType)){
-            $meals = $meals->where('meal_type_id','like',"%$mealType%");
+            $mealTypeName = MealTypeModel::find($mealType)?->name ?? $mealType; // Lấy tên diet từ DB
+            $meals = $meals->where('meals.meal_type_id',$mealType);
+            if($mealTypeName){
+                    $searchConditions[] = 'chế độ ăn " ' . $mealTypeName. ' "';
+
+                }
         }
 
-
-        
+        //lọc theo calories
+        if(!empty($caloriesMin )){
+            $meals = $meals->having('total_calories','>=',$caloriesMin);
+                    
+            $searchConditions[] = 'Calories Min " ' . $caloriesMin. ' "';
+        }
+        if(!empty($caloriesMax)){
+            $meals = $meals->having('total_calories','<=',$caloriesMax);
+            $searchConditions[] = 'Calories Max " ' . $caloriesMax. ' "';
+        }
 
         // lay du lieu phan trang
         $meals = $meals->paginate(9,'*','meals_page');
@@ -41,7 +112,15 @@ class MealsController extends BaseController
         return view($this->pathViewController.'index',[
             'meals'=>$meals,
             'search'=>$search,
-            'tab'=>$tab,
+            'diet'=>$diet,
+            'mealType'=>$mealType,
+            'caloriesMin'=>$caloriesMin,
+            'caloriesMax'=>$caloriesMax,
+            'allergen'=>$allergen,
+            'dietTypes'=>$dietTypes,
+            'mealTypes'=>$mealTypes,
+            'allergens'=> $allergens,
+            'searchConditions'=>$searchConditions
         ]);
     }
 
--- resources/views/site/meals/index.blade.php
+++ resources/views/site/meals/index.blade.php
@@ -1,6 +1,7 @@
 @extends('site.layout')
 
 @section('content')
+    {{-- style --}}
     <style>
         .col-md-4 {
             padding-bottom: 30px; /* tạo khoảng trống dưới card */
@@ -44,35 +45,93 @@
     </section>
     <div class="card shadow-sm">
         <div class="card-body">
-             {{-- form lọc --}}
-            <form action="{{route('meal.index')}}" class="mb-4" method="GET">
-                <div class="row">
-                    <div class="col-md-4">
-                        <input type="text" name="search" value{{$search}} class="form-control" value="{{$search ?? old($search)}}" placeholder="Tìm món ăn, nguyên liệu hoặc chế độ ăn...">
+             {{-- form lọc + fillter--}}
+             <div class="card-header">
+                <form action="{{route('meal.index')}}" class="mb-4" method="GET">
+                    <div class="row g-2">
+                        {{-- search --}}
+                        <div class="col-md-5">
+                            <input type="text" name="search" class="form-control @error('search') is-invalid @enderror" value="{{$search ?? old($search)}}" placeholder="Tìm món ăn, nguyên liệu hoặc chế độ ăn...">
+                            {{-- Hiển thị thông báo lỗi --}}
+                            @error('search')
+                                <p class="text-danger">
+                                    {{ $message }}
+                                </p>
+                            @enderror
+                        </div>
+                        {{-- calo min --}}
+                        <div class="col-md-2">
+                            <input type="number" name="calories_min" class="form-control" id="" value="{{$caloriesMin ?? old($caloriesMin)}}" placeholder="Nhập Calories Min...">
+                        </div >
+                        {{-- calo max --}}
+                        <div class="col-md-2">
+                            <input type="number" name="calories_max" class="form-control" id="" value="{{$caloriesMax ?? old($caloriesMax)}}" placeholder="Nhập Calories Max...">
+                        </div>
+                        {{-- Submit button --}}
+                        <div class="col-md-2 ">
+                            <button class="btn btn-primary w-100">Tìm</button>
+                        </div>
+                    </div>
+                    <div class="row g-2 mt-2 align-items-center">
+                        <div class="d-flex align-items-center mb-4">
+                        {{-- <ul class="nav nav-tabs mb-4">
+                            <li class="nav-item">
+                                <a href="{{ route('meal.index', ['tab' => 'thuc-don']) }}" class="nav-link active fw-bold text-dark" style="border-bottom:3px solid red; display: inline-block; padding-bottom: 4px;">Thực đơn</a>
+                            </li>
+                        </ul> --}}
+                        {{-- <input type="submit" name="tab" value="{{ request('tab', 'thuc-don') }}" class="m-2"> --}}
+                        
+                        {{-- fillter  diettype --}}
+                        <div class="col-md-3">
+                            <select name="diet" class="form-select me-2" onchange="this.form.submit()">
+                                <option value="">-- Chọn Diet Type --</option>
+                                @foreach($dietTypes as $diet)
+                                    <option value="{{ $diet->id }}" {{ request('diet') == $diet->id ? 'selected' : '' }}>
+                                        {{ $diet->name }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
+
+                        {{-- fillter  mealTypeFill--}}
+                        <div class="col-md-3">
+                            <select name="meal_type" class="form-select me-2" onchange="this.form.submit()">
+                                <option value="">-- Chọn Meal Type --</option>
+                                @foreach($mealTypes as $mealType)
+                                    <option value="{{ $mealType->id }}" {{ request('meal_type') == $mealType->id ? 'selected' : '' }}>
+                                        {{ $mealType->name }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
+
+                        {{-- fillter  allergenFill--}}
+                        <div class="col-md-3">
+                            <select name="allergen" class="form-select" onchange="this.form.submit()">
+                                <option value="">-- Chọn Allergen --</option>
+                                @foreach($allergens as $allergen)
+                                    <option value="{{ $allergen->id }}" {{ request('allergen') == $allergen->id ? 'selected' : '' }}>
+                                        {{ $allergen->name }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
                     </div>
                     
-                    <div class="col-md-2">
-                        <button class="btn btn-primary w-100">Tìm</button>
-                    </div>
-                </div>
-            </form>
-
-            {{-- danh sách meals --}}
-            <div class="d-flex align-items-center mb-4">
-                <ul class="nav nav-tabs mb-4">
-                    <li class="nav-item">
-                        <a href="{{ route('meal.index', ['tab' => 'thuc-don']) }}" class="nav-link active fw-bold text-dark" style="border-bottom:3px solid red; display: inline-block; padding-bottom: 4px;">Thực đơn</a>
-                        
-                    </li>
-                </ul>
+                    
+                </form>
                 
-            </div>
+             </div>
+
+            
             <div class="card shadow-sm p-3">
                 <!-- <div class=" mb-3 d-flex justify-content-around">
                     <a href="#" class="btn btn-outline-secondary">&laquo; Tuần rước</a>
                     <p>11.08 <span>-</span> 17.08</p>
                     <a href="#" class="btn btn-outline-secondary">Tuần Sau &raquo; </a>
                 </div> -->
+
+
                 @if( $meals->count() > 0)
                     
                     <div class="row mb-3">
@@ -82,14 +141,42 @@
                                     $totalCarbs= 0;
                                     $totalFat= 0;
                                     $totalKcal= 0;
-                                    foreach($meal->recipeIngredients as $pri){
-                                        $ingredient = $pri->ingredient;
-                                        if($ingredient){
-                                            $totalPro += $ingredient->protein;
-                                            $totalCarbs += $ingredient->carb;
-                                            $totalFat += $ingredient->fat;
-                                            $totalKcal += ($ingredient->protein*4) + ($ingredient->carb*4) + ($ingredient->fat*9);
+                                    // foreach($meal->recipeIngredients as $pri){
+                                    //     $ingredient = $pri->ingredient;
+                                    //     // if($ingredient){
+                                    //     //     $totalPro += $ingredient->protein;
+                                    //     //     $totalCarbs += $ingredient->carb;
+                                    //     //     $totalFat += $ingredient->fat;
+                                    //     //     $totalKcal += ($ingredient->protein*4) + ($ingredient->carb*4) + ($ingredient->fat*9);
+                                    //     // }
+                                    //     // lấy total_calo từ recipe_ingredient 
+                                    //     if($pri->total_calo){
+                                    //         $totalKcal += $pri->total_calo;
+                                    //     }
+                                    // }
+                                    // // nếu không có total_calo trong db, trở về cách tính cũ 
+                                    // if($totalKcal == 0){
+                                    //     foreach($meal->recipeIngredients as $recipeIngredient){
+                                    //         $ingredient = $recipeIngredient->ingredient;
+                                    //         if($ingredient){
+                                    //             $totalKcal += ($ingredient->protein*4) + ($ingredient->carb*4) + ($ingredient->fat*9);
+                                    //         }
+                                    //     }
+                                    // }
+
+                                    foreach ($meal->recipeIngredients as $ing) {
+                                        // Ưu tiên dùng giá trị đã tính sẵn trong DB (nếu có)
+                                        if (isset($ing->total_calo)) {
+                                            $totalKcal += $ing->total_calo;
+                                        } else {
+                                            // Tính thủ công nếu không có sẵn
+                                            $totalKcal += ($ing->ingredient->protein * 4 + $ing->ingredient->carb * 4 + $ing->ingredient->fat * 9) * ($ing->quantity ?? 1);
                                         }
+                                        
+                                        // Tính protein, carb, fat (bắt buộc tính thủ công nếu không lưu sẵn)
+                                        $totalPro += ($ing->ingredient->protein ?? 0) * ($ing->quantity ?? 1);
+                                        $totalCarbs += ($ing->ingredient->carb ?? 0) * ($ing->quantity ?? 1);
+                                        $totalFat += ($ing->ingredient->fat ?? 0) * ($ing->quantity ?? 1);
                                     }
                                 @endphp
                             
@@ -126,24 +213,32 @@
                             
                         @endforeach
                     </div>
-                    @else
-                        <div class="alert alert-warning text-center mx-auto" style="width: 40%">
-                            
-                             @if(!empty($search)&& !empty($mealTypeName))
-                               Không có kết quả tìm kiếm nào  "<strong>{{ $search }}</strong>" và loại "<strong>{{ $mealTypeName }}</strong>"
-                            @elseif(!empty($mealTypeName))
-                                Không có món ăn nào cho loại "<strong>{{ $mealTypeName }}</strong>"
-                            @elseif(!empty($search))
-                                Không có kết quả tìm kiếm cho "<strong>{{ $search }}</strong>".
+                @else
+                    <div class="alert alert-warning text-center mx-auto" style="width: 40%">
+                        
+                            {{-- @if(!empty($search)&& !empty($mealTypeName))
+                            Không có kết quả tìm kiếm nào  "<strong>{{ $search }}</strong>" và loại "<strong>{{ $mealTypeName }}</strong>"
+                        @elseif(!empty($mealTypeName))
+                            Không có món ăn nào cho loại "<strong>{{ $mealTypeName }}</strong>"
+                        @elseif(!empty($search))
+                            Không có kết quả tìm kiếm cho "<strong>{{ $search }}</strong>".
+                        @endif --}}
+                         <div class="alert alert-warning">
+                            Không có kết quả hiển thị  
+                            @if (!empty($searchConditions))
+                                cho {{ implode(' và ', $searchConditions) }}
                             @endif
                         </div>
-                    @endif
+                    </div>
+                @endif
                 
-
-            {{--phan trang  --}}
-            <div>
-                {{$meals->appends(request()->except('meals_page'))->links('pagination::bootstrap-5')}}
+                {{--phan trang  --}}
+                <div>
+                    {{$meals->appends(request()->except('meals_page'))->links('pagination::bootstrap-5')}}
+                </div>
             </div>
+
+            
         </div>
 
     </div>
