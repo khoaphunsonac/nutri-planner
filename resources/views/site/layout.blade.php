cd7b6345168a177ff510c029fecf4bf3d77b437a
unknown <dipthanhhong1020@gmail.com>
2025-08-17 17:25:58 +0700

    cập nhật like và phần giao diện

--- resources/views/site/layout.blade.php
+++ resources/views/site/layout.blade.php
@@ -135,22 +135,7 @@
                             @endif
                         </a>
                     </div>
-                    {{-- Popup --}}
-                    {{-- <div id="loginRegisterPopup" style="display:none; position:fixed; top:0; left:0; width:100%; height:100%; background:rgba(0,0,0,0.5); z-index:9999;">
-                        <div style="background:white; padding:20px; border-radius:8px; width:500px; margin:150px auto; text-align:center; position:relative;">
-                            <h4>Bạn cần đăng nhập hoặc đăng ký</h4>
-                            <p>Hãy chọn một trong hai để tiếp tục</p>
-                            <div style="margin-top:15px;">
-                                <a href="{{ route('login') }}" class="btn btn-primary" style="margin-right:5px;">
-                                    <i class="bi bi-lock"></i> Đăng nhập
-                                </a>
-                                <a href="{{ route('register.submit') }}" class="btn btn-success">
-                                    <i class="bi bi-person-plus-fill me-2"></i> Đăng ký
-                                </a>
-                            </div>
-                            <button onclick="closeLoginRegisterPopup()" style="position:absolute; top:5px; right:8px; background:none; border:none; font-size:18px; cursor:pointer;">×</button>
-                        </div>
-                    </div> --}}
+                    
                 </div>
 
 
@@ -217,20 +202,7 @@
             });
         });
 
-        // /* Script xử lý giỏ hàng */
-        // function handleCartClick() {
-        //     @if(auth()->check())
-        //         // Nếu đã đăng nhập → chuyển đến trang giỏ hàng
-        //         window.location.href = "{{ route('meal.showsavemeals') }}";
-        //     @else
-        //         // Nếu chưa đăng nhập → hiện popup
-        //         document.getElementById('loginRegisterPopup').style.display = 'block';
-        //     @endif
-        // }
-
-        // function closeLoginRegisterPopup() {
-        //     document.getElementById('loginRegisterPopup').style.display = 'none';
-        // }
+        
     </script>
 </body>
 
