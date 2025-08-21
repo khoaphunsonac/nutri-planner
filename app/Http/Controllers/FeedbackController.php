be977413092f8bd365091a9d3c1bf88abae48d50
MINHKHANGHO <WinsTV09@gmai.com>
2025-08-21 08:26:17 +0700

    chinh lai sitefeedback

--- app/Http/Controllers/FeedbackController.php
+++ app/Http/Controllers/FeedbackController.php
@@ -1,67 +1,33 @@
 <?php
 
-namespace App\Http\Controllers\Admin;
+namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\FeedbackModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\FeedbackRequest;
 
 class FeedbackController extends Controller
 {
-    public function index(Request $request)
+
+
+    // Form gửi feedback
+    public function create()
     {
-        $query = FeedbackModel::with('account');
-
-        if ($request->filled('search')) {
-            $query->where('comment', 'like', '%' . $request->search . '%');
-        }
-        if ($request->filled('rating')) {
-            $query->where('rating', $request->rating);
-        }
-
-        if ($request->filled('status')) {
-            $query->where('status', $request->status);
-        }
-        // Lọc theo khoảng ngày
-        if ($request->filled('date_from') && $request->filled('date_to')) {
-            $query->whereBetween('created_at', [
-                $request->date_from . " 00:00:00",
-                $request->date_to . " 23:59:59"
-            ]);
-        } elseif ($request->filled('date_from')) {
-            $query->whereDate('created_at', '>=', $request->date_from);
-        } elseif ($request->filled('date_to')) {
-            $query->whereDate('created_at', '<=', $request->date_to);
-        }
-
-        // $feedbacks = FeedbackModel::all();
-        $feedbacks = $query->orderBy('created_at', 'desc')->paginate(10);
-        // return $feedbacks;
-        return view('Admin.feedbacks.index', [
-            'feedbacks' => $feedbacks,
-            'search' => $request->search,
-            'status' => $request->status,
-        ]);
+        return view('site.feedbacks.create');
     }
 
-    // Xem chi tiết phản hồi
-    public function show($id)
+    // Lưu feedback
+    public function store(FeedbackRequest $request)
     {
-        $feedback = FeedbackModel::with('account')->findOrFail($id);
-        return view('Admin.feedbacks.show', [
-            'feedback' => $feedback,
+
+        FeedbackModel::create([
+            'account_id' => Auth::user()->id,
+            'rating'     => $request->rating,
+            'comment'    => $request->comment,
         ]);
-    }
 
-
-
-
-    // Xóa phản hồi
-    public function destroy($id)
-    {
-        $feedback = FeedbackModel::findOrFail($id);
-        $feedback->delete();
-
-        return redirect()->route('feedbacks.index')->with('success', 'Đã xóa phản hồi.');
+        return redirect()->route('home')->with('success', 'Cảm ơn bạn đã gửi phản hồi!');
     }
 }
